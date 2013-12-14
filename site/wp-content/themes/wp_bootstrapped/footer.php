006473f9f1fa87b2f57a85e781c7680a4f0e0ecc
jonathan finnegan <jonathan.finnegan@gmail.com>
2013-12-14 18:42:42 -0500

    bootstrap js test

--- site/wp-content/themes/wp_bootstrapped/footer.php
+++ site/wp-content/themes/wp_bootstrapped/footer.php
@@ -26,6 +26,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
 	<script src=""<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js" type="text/javascript"></script>
 
 <?php wp_footer(); ?>
   </body>
